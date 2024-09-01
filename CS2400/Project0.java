e3ff068de0bf99f873fbe3ab43868e76ab8c2b19
aaronaramburo <aaronaramburo7463@gmail.com>
2024-09-01 16:53:19 -0700

    Added while loop to allow program to repeat based on user input

--- CS2400/Project0.java
+++ CS2400/Project0.java
@@ -3,25 +3,27 @@ import java.util.Scanner;
 public class Project0 {
 
     public static void main(String[] args) {
-       Scanner scanner = new Scanner(System.in);
-       System.out.println("Welcome to the Calculator.");
-       System.out.println("Enter 'a' to add, 's' to subtract, or 'q' to quit: ");
+        Scanner scanner = new Scanner(System.in);
+        
+        while (true){
+            System.out.println("Welcome to the Calculator.");
+            System.out.println("Enter 'a' to add, 's' to subtract, or 'q' to quit: ");
+            
+            String userInput = scanner.nextLine();
+            
+            if (userInput.equals("a")){
+                add();
+            } else if (userInput.equals("s")){
+                subtract();
+            } else if (userInput.equals("q")){
+                System.out.println("Goodbye!");
+                break;
+            } else {
+                System.out.println("Invalid input, try again.");
+            }
+        }
        
-       String userInput = scanner.nextLine();
-
-       if (userInput.equals("a")){
-        add();
-       } else if (userInput.equals("s")){
-        subtract();
-       } else if (userInput.equals("q")){
-        System.out.println("Goodbye!");
-        System.exit(0);
-       } else {
-        System.out.println("Invalid input, try again.");
-       }
-
-
-
+        scanner.close();
 
     }
 
@@ -36,6 +38,7 @@ public class Project0 {
 
     int sum = numberOne + numberTwo;
     System.out.println("The sum is " + sum);
+    scanner.close();
 
     }
 
@@ -50,6 +53,7 @@ public class Project0 {
     
         int difference = numberOne - numberTwo;
         System.out.println("The difference is " + difference);
+        scanner.close();
     
         }
 
