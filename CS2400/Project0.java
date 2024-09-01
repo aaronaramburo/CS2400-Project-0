e1931afd30a123a25f9984b248789c1258931a89
aaronaramburo <aaronaramburo7463@gmail.com>
2024-09-01 16:42:04 -0700

    Added subtraction function

--- CS2400/Project0.java
+++ CS2400/Project0.java
@@ -5,12 +5,14 @@ public class Project0 {
     public static void main(String[] args) {
        Scanner scanner = new Scanner(System.in);
        System.out.println("Welcome to the Calculator.");
-       System.out.println("Enter 'a' to add or 'q' to quit: ");
+       System.out.println("Enter 'a' to add, 's' to subtract, or 'q' to quit: ");
        
        String userInput = scanner.nextLine();
 
        if (userInput.equals("a")){
         add();
+       } else if (userInput.equals("s")){
+        subtract();
        } else if (userInput.equals("q")){
         System.out.println("Goodbye!");
         System.exit(0);
@@ -36,5 +38,19 @@ public class Project0 {
     System.out.println("The sum is " + sum);
 
     }
+
+    public static void subtract(){
+        Scanner scanner = new Scanner(System.in);
+    
+        System.out.println("Enter the first number: ");
+        int numberOne = scanner.nextInt();
+    
+        System.out.println("Enter the second number: ");
+        int numberTwo = scanner.nextInt();
+    
+        int difference = numberOne - numberTwo;
+        System.out.println("The difference is " + difference);
+    
+        }
 
 }
